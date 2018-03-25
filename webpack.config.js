673f6b8e47de96d6f5ebf014288770e1298b9b24
Jason Borer <borerjason@gmail.com>
2018-03-25 16:40:33 -0700

    add jest config

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,13 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "js/[name].js"
   },
+  resolve: {
+    extensions: [' ', '.js', '.jsx'],
+  },
   module: {
     rules: [
       {
-        test:/\.js$/,
+        test:/\.js#/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
