26dfc51cd1908beaee83e7e670ab435ed5fd4486
Jason Borer <borerjason@gmail.com>
2018-03-25 20:10:46 -0700

    move server to src

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,13 @@ module.exports = {
         }
       },
       {
+        test:/\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
         test: /\.html$/,
         use: [
           {
